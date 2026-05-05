3e88bba251992083a4a4f03dd1733d9f8e1da1e0
DIVYA <131283175+DIVICODER@users.noreply.github.com>
2026-05-05 23:26:54 +0530

    Delete GemConnect directory

--- GemConnect/gemconnect/__init__.py
+++
@@ -1,9 +0,0 @@
-from .connector import ask_gemini
-__version__ = "1.0.0"
-
-def list_models(client):
-    print("The available Gemini Models are:")
-    for model in client.models.list():
-        print(model.name)
-
-    
--- GemConnect/test.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-from gemconnect import list_models
-from gemconnect.connector import ask_gemini
-
-client = genai.Client(api_key="YOUR API KEY")
-
-result = ask_gemini("What is Python?", client, "gemini-2.5-flash")
-print(result)
-# list_models(client)
