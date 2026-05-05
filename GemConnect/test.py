ff0985d126737b2a4608a3fd9544c340a64bad75
DIVYA <131283175+DIVICODER@users.noreply.github.com>
2026-05-05 17:13:27 +0530

    Update test.py

--- GemConnect/test.py
+++ GemConnect/test.py
@@ -2,7 +2,7 @@ from google import genai
 from gemconnect import list_models
 from gemconnect.connector import ask_gemini
 
-client = genai.Client(api_key="AIzaSyABJfEl-cemtMt7fDVgmjI7Cp2vshIx3Rs")
+client = genai.Client(api_key="YOUR API KEY")
 
 result = ask_gemini("What is Python?", client, "gemini-2.5-flash")
 print(result)
